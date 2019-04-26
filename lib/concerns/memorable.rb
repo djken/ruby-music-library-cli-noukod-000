8ca3c8fda6eb14c698a7c41656a08a0618bff96e
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-26 19:49:48 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,19 @@
-module Concerns::Memorable
+module Memorable
   module ClassMethods
     def destroy_all
       self.all.clear
     end
 
-    module InstanceMethods
+    def create(name)
+      object = self.new(name).tap do |object|
+        object.save
+      end
+    end
+  end
+
+  module InstanceMethods
     def save
       self.class.all << self
     end
+  end
 end
