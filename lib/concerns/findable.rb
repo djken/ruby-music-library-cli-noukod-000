3a8f20495ac34053c48018a7d7f55f7d966ff91f
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-26 16:33:45 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable
 
-  def find_by_name(f_name)
+  def find_by_name(f_name) # Allow to find an instance of the class by name.
      all.detect do |song_dectect|
        song_dectect.name == f_name
      end
