24179d672b3ec3cc84efbc5abff90b4991cf846f
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-26 19:35:07 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -13,7 +13,7 @@ module Concerns::Findable
       self.find_by_name(name) || self.create(name)
   end
 
-  # def create(name)
-  #   object = self.new(name).tap {|object| object.save}
-  # end
+  def create(namee)
+    object = self.new(name).tap {|object| object.save}
+  end
 end
